05b98c209861a3fc860a9ca1894a5a8f91a646c2
Paddy <ohanlon.paddy@gmail.com>
2022-12-23 12:46:11 +0100

    Try add CA

--- db/index.js
+++ db/index.js
@@ -2,10 +2,16 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+console.log("process.env", process.env);
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV !== 'production' ? false : {
+    rejectUnauthorized: false,
+    ca: process.env.CA_CERT
+  }
 });
 
 module.exports = {
